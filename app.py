0f172fef78bc84fc7540ee143438539dbb0d0bd5
Nikita Korolev <62380520+iratewarrior@users.noreply.github.com>
2024-07-11 14:36:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,16 +2,6 @@ import pandas as pd
 import streamlit as st
 import base64
 
-
-@st.experimental_memo
-def get_img_as_base64(file):
-    with open(file, "rb") as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-
-
-img = get_img_as_base64("image.jpg")
-
 page_bg_img = f"""
 <style>
 [data-testid="stAppViewContainer"] > .main {{
